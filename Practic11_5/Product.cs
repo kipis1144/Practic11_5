01e8701745aa677aba2f9bfa3cacb4df4bcfb1a6
Kirill <kipis1144@gmail.com>
2025-10-26 23:17:09 +0300

    Добавьте файлы проекта.

---
+++ Practic11_5/Product.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic11_5
+{
+    /* internal class Product
+     {
+         private int price;
+         private int discount;
+
+
+         public int Price { 
+             get { return price; } 
+             set { price = value >= 0 ? value : 0; } 
+         }
+         public int Discount
+         {
+             get { return discount; }
+             set { discount = value >= 0 && value <= 100 ? value : 0; }
+         }
+         public int FinalPrice() {
+             get { return Price * (1 - Discount / 100); }
+         }
+     }*/
+
+
+    class Product
+    {
+        private double price;
+        private double discount;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value >= 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Цена не может быть отрицательной!");
+                }
+            }
+        }
+        public double Discount
+        {
+            get { return discount; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    discount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Скидка должна быть в диапазоне 0-100%.");
+                }
+            }
+        }
+        public double FinalPrice
+        {
+            get { return price * (1 - discount / 100); }
+        }
+        public string Name { get; set; } = "Без названия";
+        public Product(double price, double discount)
+        {
+            Price = price;
+            Discount = discount;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"{Name}: {FinalPrice:F0} (скидка {Discount}%)");
+        }
+    }
+
+}
---
+++ Practic11_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic11_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Phone = new Product(2000, 30);
+            Phone.Show();
+            Phone.Discount = 10;
+            Phone.Show();
+        }
+    }
+}
